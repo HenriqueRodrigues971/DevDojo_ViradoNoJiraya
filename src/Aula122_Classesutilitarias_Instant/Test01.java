4232b3f1e84c84a4c0be2b533854e9ce154ad853
HenriqueRodrigues971 <kingofsneaker123@gmail.com>
2024-05-16 01:34:36 -0300

    up

---
+++ src/Aula122_Classesutilitarias_Instant/Test01.java
@@ -0,0 +1,17 @@
+package Aula122_Classesutilitarias_Instant;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+public class Test01 {
+	public static void main(String[] args) {
+		Instant now = Instant.now();
+		System.out.println(now);
+		System.out.println(LocalDateTime.now());
+		System.out.println(now.getEpochSecond());
+		System.out.println(now.getNano());
+		System.out.println(Instant.ofEpochSecond(3));
+		System.out.println(Instant.ofEpochSecond(3,1000000000));
+		System.out.println(Instant.ofEpochSecond(3,-1000000000));
+	}
+}
